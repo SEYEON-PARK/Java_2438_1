5368fc4f7010a96d0aa1800e061d9a49c4fc17a7
SEYEON-PARK <62700321+SEYEON-PARK@users.noreply.github.com>
2021-07-29 01:22:30 +0900

    Java언어 BAEKJOON 2438번 문제 풀기!(1)

--- Java_2438_1.java
+++ Java_2438_1.java
@@ -6,13 +6,13 @@
 첫째 줄부터 N번째 줄까지 차례대로 별을 출력한다.
 */
 
-import java.util.Scanner;
+import java.util.Scanner; // 스캐너 사용을 위해서
 
-public class Main{
-    public static void main(String arg[]){
-        Scanner a = new Scanner(System.in);
-        int N=a.nextInt();
-        int num=1;
+public class Main{ // Main 클래스
+    public static void main(String arg[]){ // main() 메소드
+        Scanner a = new Scanner(System.in); // 스캐너 a 생성
+        int N=a.nextInt(); // 사용자로부터 정수 입력받기
+        int num=1; // 별의 개수를 저장할 변수 선언 및 초기화
         
         for(int i=0;i<N;i++){
             for(int j=0;j<num;j++){
